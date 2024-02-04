237b85cda12f62ef864a9ec28f1e548bc694b2e0
jiakaiqiang <13165792976@163.com>
2024-02-04 09:59:56 +0800

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, HttpCode } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+  @Get()
+  @HttpCode(204)
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
